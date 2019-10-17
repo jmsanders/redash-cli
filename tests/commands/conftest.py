ee685568c92053fab9aeeaaebed6c7bbacad358c
Jordan Sanders <jordan.sanders@joinroot.com>
2019-10-17 16:57:11 -0500

    Extract MockClient into conftest.py
    Signed-off-by: Jesse Cooper <jesse.cooper@joinroot.com>

--- tests/commands/conftest.py
+++ tests/commands/conftest.py
@@ -2,6 +2,23 @@ import pytest
 from click.testing import CliRunner
 
 
+class MockClient:
+    def __init__(self, return_value):
+        self.return_value = return_value
+
+    def get(self, endpoint, limit=None):
+        self.called_endpoint = endpoint
+        return self.return_value
+
+
 @pytest.fixture
 def cli_runner():
     return CliRunner()
+
+
+@pytest.fixture
+def mock_client():
+    def mock_client_factory(return_value):
+        return MockClient(return_value)
+
+    return mock_client_factory
--- tests/commands/test_list.py
+++ tests/commands/test_list.py
@@ -7,36 +7,27 @@ import responses
 from redash.commands.list import list_
 
 
-class MockClient:
-    def __init__(self, return_value):
-        self.return_value = return_value
-
-    def get(self, endpoint, limit=None):
-        self.called_endpoint = endpoint
-        return self.return_value
-
-
-def test_list_queries(cli_runner):
+def test_list_queries(cli_runner, mock_client):
     expected = "result"
-    result = cli_runner.invoke(list_, ["queries"], obj=MockClient(expected))
+    result = cli_runner.invoke(list_, ["queries"], obj=mock_client(expected))
 
     assert not result.exception
     assert expected in result.stdout
 
 
-def test_list_queries_with_limit(cli_runner):
+def test_list_queries_with_limit(cli_runner, mock_client):
     expected = "result"
     result = cli_runner.invoke(
-        list_, ["queries", "--limit", 1], obj=MockClient(expected)
+        list_, ["queries", "--limit", 1], obj=mock_client(expected)
     )
 
     assert not result.exception
     assert expected in result.stdout
 
 
-def test_list_my_queries(cli_runner):
+def test_list_my_queries(cli_runner, mock_client):
     expected = "result"
-    client = MockClient(expected)
+    client = mock_client(expected)
     result = cli_runner.invoke(list_, ["queries", "--mine", True], obj=client)
 
     assert not result.exception
@@ -44,9 +35,9 @@ def test_list_my_queries(cli_runner):
     assert client.called_endpoint == "queries/my"
 
 
-def test_list_my_queries_by_default(cli_runner):
+def test_list_my_queries_by_default(cli_runner, mock_client):
     expected = "result"
-    client = MockClient(expected)
+    client = mock_client(expected)
     result = cli_runner.invoke(list_, ["queries"], obj=client)
 
     assert not result.exception
@@ -54,9 +45,9 @@ def test_list_my_queries_by_default(cli_runner):
     assert client.called_endpoint == "queries/my"
 
 
-def test_list_everybodys_queries(cli_runner):
+def test_list_everybodys_queries(cli_runner, mock_client):
     expected = "result"
-    client = MockClient(expected)
+    client = mock_client(expected)
     result = cli_runner.invoke(list_, ["queries", "--mine", False], obj=client)
 
     assert not result.exception
@@ -64,9 +55,9 @@ def test_list_everybodys_queries(cli_runner):
     assert client.called_endpoint == "queries"
 
 
-def test_list_data_sources(cli_runner):
+def test_list_data_sources(cli_runner, mock_client):
     expected = "result"
-    result = cli_runner.invoke(list_, ["data-sources"], obj=MockClient(expected))
+    result = cli_runner.invoke(list_, ["data-sources"], obj=mock_client(expected))
 
     assert not result.exception
     assert expected in result.stdout
