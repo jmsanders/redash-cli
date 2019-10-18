20937fe9eeeea306f5ac92bcd9e4ec5875eb641f
Jordan Sanders <jordan.sanders@joinroot.com>
2019-10-18 10:22:05 -0500

    Ensure all commands are added to cli
    Signed-off-by: Jesse Cooper <jesse.cooper@joinroot.com>

---
+++ tests/test_cli.py
@@ -0,0 +1,8 @@
+from redash.cli import cli
+
+
+def test_commands_added_to_cli():
+    assert "configure" in cli.commands.keys()
+    assert "edit" in cli.commands.keys()
+    assert "list" in cli.commands.keys()
+    assert "new" in cli.commands.keys()
