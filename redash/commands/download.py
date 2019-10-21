acefa88c4afb1fda9b4ba6d6a4f0c9a678b11c13
Jordan Sanders <jordan.sanders@joinroot.com>
2019-10-21 17:10:53 -0500

    Download the latest query if no query-id is provided

--- redash/commands/download.py
+++ redash/commands/download.py
@@ -4,9 +4,11 @@ from redash import utils
 
 
 @click.command(help="Execute a query and get the results.")
-@click.option("--query-id", type=int, required=True, help="ID of query to execute.")
+@click.option("--query-id", type=int, help="ID of query to execute.")
 @click.pass_obj
 def download(client, query_id):
+    if not query_id:
+        query_id = client.get("queries/my", limit=1)[0].get("id")
     utils.echo(perform_download(client, query_id))
 
 
