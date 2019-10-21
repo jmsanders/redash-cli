5f4b9b1fe5ba6b18d7c752e875b4eaa714f50a87
Jordan Sanders <jordan.sanders@joinroot.com>
2019-10-21 16:14:12 -0500

    Revert "Edit queries in $EDITOR"
    This reverts commit 8c2147b429f4298a9356659d6be4bb37fd3eae37.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,25 +4,6 @@ from redash.config import Config
 from redash.client import RedashClient
 
 
-class MockClient:
-    def __init__(self, return_values):
-        self.return_values = (
-            return_values if isinstance(return_values, list) else [return_values]
-        )
-
-    def get(self, endpoint, limit=None):
-        self.called_endpoint = endpoint
-        return self.return_values.pop(0)
-
-    def post(self, endpoint, payload=None):
-        self.called_endpoint = endpoint
-        self.recorded_payload = payload
-        return self.return_values.pop(0)
-
-    def poll(self, endpoint):
-        self.called_endpoint = endpoint
-        return self.return_values.pop(0)
-
 @pytest.fixture
 def config(tmp_path):
     config = Config(tmp_path / "config.ini")
@@ -34,11 +15,3 @@ def config(tmp_path):
 @pytest.fixture
 def client(config):
     return RedashClient(config)
-
-    
-@pytest.fixture
-def mock_client():
-    def mock_client_factory(return_value):
-        return MockClient(return_value)
-
-    return mock_client_factory
