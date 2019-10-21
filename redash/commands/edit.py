2f6060f1b83213b53bef52e59d15284186eb9441
Jordan Sanders <jordan.sanders@joinroot.com>
2019-10-21 10:06:24 -0500

    Automatically execute a query after editing it
    Signed-off-by: Jesse Cooper <jesse.cooper@joinroot.com>

--- redash/commands/edit.py
+++ redash/commands/edit.py
@@ -2,10 +2,23 @@ import json
 
 import click
 
+from redash.commands.download import perform_download
+
 
 @click.command(help="Edit and resubmit an existing query.")
 @click.option("--query-id", type=int, required=True, help="ID of query to edit.")
 @click.option("--query", type=str, required=True, help="SQL query string.")
+@click.option(
+    "--execute",
+    type=bool,
+    default=True,
+    help="Execute the query and download its results.",
+    show_default=True,
+)
 @click.pass_obj
-def edit(client, query_id, query):
-    print(json.dumps(client.post(f"queries/{query_id}", dict(query=query))))
+def edit(client, query_id, query, execute):
+    response = client.post(f"queries/{query_id}", dict(query=query))
+    if execute:
+        print(json.dumps(perform_download(client, query_id)))
+    else:
+        print(json.dumps(response))
--- tests/commands/test_edit.py
+++ tests/commands/test_edit.py
@@ -3,10 +3,39 @@ from redash.commands.edit import edit
 
 def test_edit_query(cli_runner, mock_client):
     query_id = 12345
+    expected_new_query_response = dict(id=query_id)
+    job_id_response = dict(job=dict(id=1))
+    query_result_id = 1
+    query_result_id_response = dict(job=dict(query_result_id=query_result_id))
+    download_response = "Success"
+
+    client = mock_client(
+        [
+            expected_new_query_response,
+            job_id_response,
+            query_result_id_response,
+            download_response,
+        ]
+    )
+    result = cli_runner.invoke(
+        edit, ["--query-id", query_id, "--query", "select 1"], obj=client
+    )
+
+    assert not result.exception
+    assert download_response in result.stdout
+    assert (
+        client.called_endpoint == f"queries/{query_id}/results/{query_result_id}.json"
+    )
+
+
+def test_edit_query_skip_execution(cli_runner, mock_client):
+    query_id = 12345
     expected = "result"
     client = mock_client(expected)
     result = cli_runner.invoke(
-        edit, ["--query-id", query_id, "--query", "select 1"], obj=client
+        edit,
+        ["--query-id", query_id, "--query", "select 1", "--execute", False],
+        obj=client,
     )
 
     assert not result.exception
