f5e1003e486edd0ce5fbaa21c325af964bd90e64
Jordan Sanders <jordan.sanders@joinroot.com>
2019-10-21 10:04:23 -0500

    Extract perform_download function
    Click documentation recommends not invoking commands directly:
    http://click.palletsprojects.com/en/5.x/advanced/#invoking-other-commands
    
    This new function can be called from other commands that need to
    execute and download results.
    
    Signed-off-by: Jesse Cooper <jesse.cooper@joinroot.com>

--- redash/commands/download.py
+++ redash/commands/download.py
@@ -7,6 +7,10 @@ import click
 @click.option("--query-id", type=int, required=True, help="ID of query to execute.")
 @click.pass_obj
 def download(client, query_id):
+    print(json.dumps(perform_download(client, query_id)))
+
+
+def perform_download(client, query_id):
     job_id = client.post(f"queries/{query_id}/refresh").get("job").get("id")
     query_result_id = client.poll(f"jobs/{job_id}").get("job").get("query_result_id")
-    print(json.dumps(client.get(f"queries/{query_id}/results/{query_result_id}.json")))
+    return client.get(f"queries/{query_id}/results/{query_result_id}.json")
